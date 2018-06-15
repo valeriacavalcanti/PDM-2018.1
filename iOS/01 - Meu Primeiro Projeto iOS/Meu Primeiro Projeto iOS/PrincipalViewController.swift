010a4172fbd6d5c145da61fdae2e771b5c1b1ee2
Valéria Cavalcanti <>
2018-06-15 15:51:20 -0300

    Add 15/06/2018

---
+++ iOS/01 - Meu Primeiro Projeto iOS/Meu Primeiro Projeto iOS/PrincipalViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meu Primeiro Projeto iOS
+//
+//  Created by IFPB on 15/06/18.
+//  Copyright © 2018 IFPB. All rights reserved.
+//
+
+import UIKit
+
+class PrincipalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
